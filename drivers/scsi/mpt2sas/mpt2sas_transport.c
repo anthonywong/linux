548272dfda03b7f04337d868ef4a59ec4853811b
Tim Gardner <tim.gardner@canonical.com>
2009-04-28 13:54:11 -0600

    mpt2sas: Change reset_type enum to avoid namespace collision.
    Preface the enum reset_type declarations with MPT2SAS_ in order to avoid
    namespace collisions with ARM arch definition, specifically SOFT_RESET.
    
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- drivers/scsi/mpt2sas/mpt2sas_transport.c
+++ drivers/scsi/mpt2sas/mpt2sas_transport.c
@@ -450,7 +450,7 @@ _transport_expander_report_manufacture(struct MPT2SAS_ADAPTER *ioc,
  issue_host_reset:
 	if (issue_reset)
 		mpt2sas_base_hard_reset_handler(ioc, CAN_SLEEP,
-		    FORCE_BIG_HAMMER);
+		    MPT2SAS_FORCE_BIG_HAMMER);
  out:
 	ioc->transport_cmds.status = MPT2_CMD_NOT_USED;
 	if (data_out)
@@ -1186,7 +1186,7 @@ _transport_smp_handler(struct Scsi_Host *shost, struct sas_rphy *rphy,
  issue_host_reset:
 	if (issue_reset) {
 		mpt2sas_base_hard_reset_handler(ioc, CAN_SLEEP,
-		    FORCE_BIG_HAMMER);
+		    MPT2SAS_FORCE_BIG_HAMMER);
 		rc = -ETIMEDOUT;
 	}
 
