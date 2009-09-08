387ce14977d329d00d80bc4830b33cbdafe0e755
Matt Wagantall <mattw@quicinc.com>
2009-09-08 11:26:56 -0700

    msm: acpuclock: Grab lock when changing ACPU freq.
    It is possible for multiple threads to concurrently execute
    acpuclk_set_rate(). To prevent race conditions, code using data from
    the drv_state struct must be locked.
    
    The mutex is acquired prior to accessing drv_state data, but only when
    acpuclk_set_rate() is called for a reason other than Power Collapse or
    SWFI. In these situations a mutex cannot be acquired because interrupts
    are disabled, and is not needed because the CPU is known to be idle.
    
    CRs-fixed: 194883
    Signed-off-by: Matt Wagantall <mattw@quicinc.com>
    Signed-off-by: David Brown <davidb@quicinc.com>

--- arch/arm/mach-msm/acpuclock-8x50.c
+++ arch/arm/mach-msm/acpuclock-8x50.c
@@ -365,22 +365,25 @@ int acpuclk_set_rate(unsigned long rate, enum setrate_reason reason)
 	struct clkctl_acpu_speed *tgt_s, *strt_s;
 	int rc = 0;
 
+	if (reason == SETRATE_CPUFREQ)
+		mutex_lock(&drv_state.lock);
+
 	strt_s = drv_state.current_speed;
 
 	if (rate == (strt_s->a11clk_khz * 1000))
-		return 0;
+		goto out;
 
 	for (tgt_s = acpu_freq_tbl; tgt_s->a11clk_khz != 0; tgt_s++) {
 		if (tgt_s->a11clk_khz == (rate / 1000))
 			break;
 	}
 
-	if (tgt_s->a11clk_khz == 0)
-		return -EINVAL;
+	if (tgt_s->a11clk_khz == 0) {
+		rc = -EINVAL;
+		goto out;
+	}
 
 	if (reason == SETRATE_CPUFREQ) {
-		mutex_lock(&drv_state.lock);
-
 		/* Increase VDD if needed. */
 		if (tgt_s->vdd > strt_s->vdd) {
 			rc = acpuclk_set_vdd_level(tgt_s->vdd);
@@ -414,7 +417,7 @@ int acpuclk_set_rate(unsigned long rate, enum setrate_reason reason)
 
 	/* Nothing else to do for SWFI. */
 	if (reason == SETRATE_SWFI)
-		return 0;
+		goto out;
 
 	if (strt_s->axiclk_khz != tgt_s->axiclk_khz) {
 		rc = ebi1_clk_set_min_rate(CLKVOTE_ACPUCLK,
@@ -425,7 +428,7 @@ int acpuclk_set_rate(unsigned long rate, enum setrate_reason reason)
 
 	/* Nothing else to do for power collapse */
 	if (reason == SETRATE_PC)
-		return 0;
+		goto out;
 
 	/* Drop VDD level if we can. */
 	if (tgt_s->vdd < strt_s->vdd) {
@@ -437,7 +440,6 @@ int acpuclk_set_rate(unsigned long rate, enum setrate_reason reason)
 out:
 	if (reason == SETRATE_CPUFREQ)
 		mutex_unlock(&drv_state.lock);
-
 	return rc;
 }
 
