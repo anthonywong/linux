f5ad2b889086b12977e670b3891ed313a458a92f
Asish Bhattacharya <asishb@qualcomm.com>
2009-11-18 19:54:34 +0530

    alsa: soc: msm: routing amixer controls for audio playback/voice
    The driver is designed to do routing configuration for audio
    playback and voice path. This also is responsible for enabling/disabling
    audio endpoints.
    
    Signed-off-by: Asish Bhattacharya <asishb@qualcomm.com>
    Signed-off-by: David Brown <davidb@quicinc.com>

---
+++ arch/arm/mach-msm/qdsp5v2/audio_dev_ctl.c
@@ -0,0 +1,386 @@
+/* Copyright (c) 2009, Code Aurora Forum. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+#include <linux/module.h>
+#include <linux/fs.h>
+#include <linux/miscdevice.h>
+#include <linux/msm_audio.h>
+#include <asm/uaccess.h>
+#include <asm/atomic.h>
+#include <mach/qdsp5v2/audio_dev_ctl.h>
+#include <linux/wait.h>
+#include <linux/sched.h>
+#include <mach/debug_audio_mm.h>
+#include <mach/qdsp5v2/qdsp5audppmsg.h>
+#include <mach/qdsp5v2/audpp.h>
+
+#define AUDIO_DEV_CTL_MAX_DEV 16
+
+struct audio_dev_ctrl_state {
+	struct mutex lock;
+	struct msm_snddev_info *devs[AUDIO_DEV_CTL_MAX_DEV];
+	u32 num_dev;
+	atomic_t opened;
+	struct msm_snddev_info *voice_rx_dev;
+	struct msm_snddev_info *voice_tx_dev;
+	wait_queue_head_t      wait;
+};
+
+static struct audio_dev_ctrl_state audio_dev_ctrl;
+
+struct audio_routing_info {
+	int running;
+	unsigned short mixer_mask[5];
+	unsigned short audrec_mixer_mask[2];
+};
+
+static struct audio_routing_info routing_info;
+
+void msm_snddev_register(struct msm_snddev_info *dev_info)
+{
+	mutex_lock(&audio_dev_ctrl.lock);
+	if (audio_dev_ctrl.num_dev < AUDIO_DEV_CTL_MAX_DEV) {
+		audio_dev_ctrl.devs[audio_dev_ctrl.num_dev] = dev_info;
+		audio_dev_ctrl.num_dev++;
+	} else
+		pr_err("%s: device registry max out\n", __func__);
+	mutex_unlock(&audio_dev_ctrl.lock);
+}
+EXPORT_SYMBOL(msm_snddev_register);
+
+int msm_snddev_devcount(void)
+{
+	return audio_dev_ctrl.num_dev;
+}
+EXPORT_SYMBOL(msm_snddev_devcount);
+
+int msm_snddev_query(int dev_id)
+{
+	if (dev_id <= audio_dev_ctrl.num_dev)
+			return 0;
+	return -ENODEV;
+}
+EXPORT_SYMBOL(msm_snddev_query);
+
+int msm_snddev_is_set(int popp_id, int copp_id)
+{
+	return routing_info.mixer_mask[popp_id] & (0x1 << copp_id);
+}
+EXPORT_SYMBOL(msm_snddev_is_set);
+
+unsigned short msm_snddev_route_enc(int enc_id)
+{
+	return routing_info.audrec_mixer_mask[enc_id];
+}
+EXPORT_SYMBOL(msm_snddev_route_enc);
+
+unsigned short msm_snddev_route_dec(int popp_id)
+{
+	return routing_info.mixer_mask[popp_id];
+}
+EXPORT_SYMBOL(msm_snddev_route_dec);
+
+int msm_snddev_set_dec(int popp_id, int copp_id, int set)
+{
+	if (set)
+		routing_info.mixer_mask[popp_id] |= (0x1 << copp_id);
+	else
+		routing_info.mixer_mask[popp_id] &= ~(0x1 << copp_id);
+
+	if (routing_info.running & (0x1 << popp_id))
+		audpp_route_stream(popp_id, routing_info.mixer_mask[popp_id]);
+	return 0;
+}
+EXPORT_SYMBOL(msm_snddev_set_dec);
+
+int msm_snddev_set_enc(int popp_id, int copp_id, int set)
+{
+	if (set)
+		routing_info.audrec_mixer_mask[popp_id] |= (0x1 << copp_id);
+	else
+		routing_info.audrec_mixer_mask[popp_id] &= ~(0x1 << copp_id);
+	return 0;
+}
+EXPORT_SYMBOL(msm_snddev_set_enc);
+
+int msm_set_voc_route(struct msm_snddev_info *dev_info, int stream_type)
+{
+	int rc = 0;
+
+	mutex_lock(&audio_dev_ctrl.lock);
+	switch (stream_type) {
+	case AUDIO_ROUTE_STREAM_VOICE_RX:
+		if (!(dev_info->capability & SNDDEV_CAP_RX) |
+		    !(dev_info->capability & SNDDEV_CAP_VOICE)) {
+			rc = -EINVAL;
+			break;
+		}
+		audio_dev_ctrl.voice_rx_dev = dev_info;
+		break;
+	case AUDIO_ROUTE_STREAM_VOICE_TX:
+		if (!(dev_info->capability & SNDDEV_CAP_TX) |
+		    !(dev_info->capability & SNDDEV_CAP_VOICE)) {
+			rc = -EINVAL;
+			break;
+		}
+		audio_dev_ctrl.voice_tx_dev = dev_info;
+		break;
+	}
+	mutex_unlock(&audio_dev_ctrl.lock);
+	return rc;
+}
+EXPORT_SYMBOL(msm_set_voc_route);
+
+int msm_get_voc_route(u32 *rx_id, u32 *tx_id)
+{
+	int rc = 0;
+
+	if (!rx_id || !tx_id)
+		return -EINVAL;
+
+	mutex_lock(&audio_dev_ctrl.lock);
+	if (!audio_dev_ctrl.voice_rx_dev || !audio_dev_ctrl.voice_tx_dev) {
+		rc = -ENODEV;
+		mutex_unlock(&audio_dev_ctrl.lock);
+		return rc;
+	}
+
+	*rx_id = audio_dev_ctrl.voice_rx_dev->acdb_id;
+	*tx_id = audio_dev_ctrl.voice_tx_dev->acdb_id;
+
+	mutex_unlock(&audio_dev_ctrl.lock);
+
+	/* wait for both tx and rx are opened */
+	rc = wait_event_timeout(audio_dev_ctrl.wait,
+		((audio_dev_ctrl.voice_rx_dev->opened == 1)
+		&& (audio_dev_ctrl.voice_tx_dev->opened == 1)),
+		30 * HZ);
+	if (!rc) {
+		pr_err("failed to get voice tx and rx opened  within 30s\n");
+		rc = -ENODEV;
+	}
+
+	return rc;
+}
+EXPORT_SYMBOL(msm_get_voc_route);
+
+struct msm_snddev_info *audio_dev_ctrl_find_dev(u32 dev_id)
+{
+	struct msm_snddev_info *info;
+
+	if ((audio_dev_ctrl.num_dev - 1) < dev_id) {
+		info = ERR_PTR(-ENODEV);
+		goto error;
+	}
+
+	info = audio_dev_ctrl.devs[dev_id];
+error:
+	return info;
+
+}
+EXPORT_SYMBOL(audio_dev_ctrl_find_dev);
+
+static int audio_dev_ctrl_get_devices(struct audio_dev_ctrl_state *dev_ctrl,
+				      void __user *arg)
+{
+	int rc = 0;
+	u32 index;
+	struct msm_snd_device_list work_list;
+	struct msm_snd_device_info *work_tbl;
+
+	if (copy_from_user(&work_list, arg, sizeof(work_list))) {
+		rc = -EFAULT;
+		goto error;
+	}
+
+	if (work_list.num_dev > dev_ctrl->num_dev) {
+		rc = -EINVAL;
+		goto error;
+	}
+
+	work_tbl = kmalloc(work_list.num_dev *
+		sizeof(struct msm_snd_device_info), GFP_KERNEL);
+	if (!work_tbl) {
+		rc = -ENOMEM;
+		goto error;
+	}
+
+	for (index = 0; index < dev_ctrl->num_dev; index++) {
+		work_tbl[index].dev_id = index;
+		work_tbl[index].dev_cap = dev_ctrl->devs[index]->capability;
+		strlcpy(work_tbl[index].dev_name, dev_ctrl->devs[index]->name,
+		64);
+	}
+
+	if (copy_to_user((void *) (work_list.list), work_tbl,
+		 work_list.num_dev * sizeof(struct msm_snd_device_info)))
+		rc = -EFAULT;
+	kfree(work_tbl);
+error:
+	return rc;
+}
+
+static int audio_dev_ctrl_ioctl(struct inode *inode, struct file *file,
+	unsigned int cmd, unsigned long arg)
+{
+	int rc = 0;
+	struct audio_dev_ctrl_state *dev_ctrl = file->private_data;
+
+	mutex_lock(&audio_dev_ctrl.lock);
+	switch (cmd) {
+	case AUDIO_GET_NUM_SND_DEVICE:
+		rc = put_user(dev_ctrl->num_dev, (uint32_t __user *) arg);
+		break;
+	case AUDIO_GET_SND_DEVICES:
+		rc = audio_dev_ctrl_get_devices(dev_ctrl, (void __user *) arg);
+		break;
+	case AUDIO_ENABLE_SND_DEVICE: {
+		struct msm_snddev_info *dev_info;
+		u32 dev_id;
+
+		if (get_user(dev_id, (u32 __user *) arg)) {
+			rc = -EFAULT;
+			break;
+		}
+		dev_info = audio_dev_ctrl_find_dev(dev_id);
+		if (IS_ERR(dev_info))
+			rc = PTR_ERR(dev_info);
+		else {
+			rc = dev_info->dev_ops.open(dev_info);
+			if (!rc)
+				dev_info->opened = 1;
+			wake_up(&audio_dev_ctrl.wait);
+		}
+		break;
+
+	}
+
+	case AUDIO_DISABLE_SND_DEVICE: {
+		struct msm_snddev_info *dev_info;
+		u32 dev_id;
+
+		if (get_user(dev_id, (u32 __user *) arg)) {
+			rc = -EFAULT;
+			break;
+		}
+		dev_info = audio_dev_ctrl_find_dev(dev_id);
+		if (IS_ERR(dev_info))
+			rc = PTR_ERR(dev_info);
+		else {
+			rc = dev_info->dev_ops.close(dev_info);
+			dev_info->opened = 0;
+		}
+		break;
+	}
+
+	case AUDIO_ROUTE_STREAM: {
+		struct msm_audio_route_config route_cfg;
+		struct msm_snddev_info *dev_info;
+
+		if (copy_from_user(&route_cfg, (void __user *) arg,
+			sizeof(struct msm_audio_route_config))) {
+			rc = -EFAULT;
+			break;
+		}
+		pr_debug("%s: route cfg %d %d type\n", __func__,
+		route_cfg.dev_id, route_cfg.stream_type);
+		dev_info = audio_dev_ctrl_find_dev(route_cfg.dev_id);
+		if (IS_ERR(dev_info)) {
+			pr_err("%s: pass invalid dev_id\n", __func__);
+			rc = PTR_ERR(dev_info);
+			break;
+		}
+
+		switch (route_cfg.stream_type) {
+
+		case AUDIO_ROUTE_STREAM_VOICE_RX:
+			if (!(dev_info->capability & SNDDEV_CAP_RX) |
+			    !(dev_info->capability & SNDDEV_CAP_VOICE)) {
+				rc = -EINVAL;
+				break;
+			}
+			dev_ctrl->voice_rx_dev = dev_info;
+			break;
+		case AUDIO_ROUTE_STREAM_VOICE_TX:
+			if (!(dev_info->capability & SNDDEV_CAP_TX) |
+			    !(dev_info->capability & SNDDEV_CAP_VOICE)) {
+				rc = -EINVAL;
+				break;
+			}
+			dev_ctrl->voice_tx_dev = dev_info;
+			break;
+		}
+		break;
+	}
+
+	default:
+		rc = -EINVAL;
+	}
+	mutex_unlock(&audio_dev_ctrl.lock);
+	return rc;
+}
+
+static int audio_dev_ctrl_open(struct inode *inode, struct file *file)
+{
+	int rc = 0;
+
+	MM_DBG("open audio_dev_ctrl\n");
+	atomic_inc(&audio_dev_ctrl.opened);
+	file->private_data = &audio_dev_ctrl;
+	return rc;
+}
+
+static int audio_dev_ctrl_release(struct inode *inode, struct file *file)
+{
+	MM_DBG("release audio_dev_ctrl\n");
+	atomic_dec(&audio_dev_ctrl.opened);
+	return 0;
+}
+
+static const struct file_operations audio_dev_ctrl_fops = {
+	.owner = THIS_MODULE,
+	.open = audio_dev_ctrl_open,
+	.release = audio_dev_ctrl_release,
+	.ioctl = audio_dev_ctrl_ioctl,
+};
+
+
+struct miscdevice audio_dev_ctrl_misc = {
+	.minor	= MISC_DYNAMIC_MINOR,
+	.name	= "msm_audio_dev_ctrl",
+	.fops	= &audio_dev_ctrl_fops,
+};
+
+static int __init audio_dev_ctrl_init(void)
+{
+	mutex_init(&audio_dev_ctrl.lock);
+	init_waitqueue_head(&audio_dev_ctrl.wait);
+
+	atomic_set(&audio_dev_ctrl.opened, 0);
+	audio_dev_ctrl.num_dev = 0;
+	audio_dev_ctrl.voice_rx_dev = NULL;
+	audio_dev_ctrl.voice_tx_dev = NULL;
+	return misc_register(&audio_dev_ctrl_misc);
+}
+
+static void __exit audio_dev_ctrl_exit(void)
+{
+}
+module_init(audio_dev_ctrl_init);
+module_exit(audio_dev_ctrl_exit);
+
+MODULE_DESCRIPTION("MSM 7K Audio Device Control driver");
+MODULE_LICENSE("GPL v2");
