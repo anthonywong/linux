ca9f3656e1bc0e3f5f8e63e49460f1406fe92015
www.kernel.org <unknown>
2009-11-25 07:14:08 -0800

    Subject: Linux 2.6.19 Patch-mainline: 2.6.19 Automatically created from "patches.kernel.org/patch-2.6.19" by xen-port-patches.py
    Acked-by: jbeulich@novell.com

--- arch/x86/kernel/acpi/boot.c
+++ arch/x86/kernel/acpi/boot.c
@@ -68,8 +68,12 @@ int acpi_strict;
 
 u8 acpi_sci_flags __initdata;
 int acpi_sci_override_gsi __initdata;
+#ifndef CONFIG_XEN
 int acpi_skip_timer_override __initdata;
 int acpi_use_timer_override __initdata;
+#else
+#define acpi_skip_timer_override 0
+#endif
 
 #ifdef CONFIG_X86_LOCAL_APIC
 static u64 acpi_lapic_addr __initdata = APIC_DEFAULT_PHYS_BASE;
@@ -1678,7 +1682,7 @@ int __init acpi_mps_check(void)
 	return 0;
 }
 
-#ifdef CONFIG_X86_IO_APIC
+#if defined(CONFIG_X86_IO_APIC) && !defined(CONFIG_XEN)
 static int __init parse_acpi_skip_timer_override(char *arg)
 {
 	acpi_skip_timer_override = 1;
