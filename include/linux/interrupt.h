b87a235215cab3726bcde9782f352878b044799c
Linux Kernel Mailing List <linux-kernel@vger.kernel.org>
2009-11-25 07:14:08 -0800

    Subject: Linux 2.6.30 This patch contains the differences between 2.6.29 and 2.6.30.
    Acked-by: Jeff Mahoney <jeffm@suse.com>
    Automatically created from "patches.kernel.org/patch-2.6.30" by xen-port-patches.py

--- include/linux/interrupt.h
+++ include/linux/interrupt.h
@@ -52,6 +52,7 @@
  * IRQF_ONESHOT - Interrupt is not reenabled after the hardirq handler finished.
  *                Used by threaded interrupts which need to keep the
  *                irq line disabled until the threaded handler has been run.
+ * IRQF_NO_SUSPEND - Prevent this interrupt from being disabled during suspend.
  */
 #define IRQF_DISABLED		0x00000020
 #define IRQF_SAMPLE_RANDOM	0x00000040
@@ -62,6 +63,7 @@
 #define IRQF_NOBALANCING	0x00000800
 #define IRQF_IRQPOLL		0x00001000
 #define IRQF_ONESHOT		0x00002000
+#define IRQF_NO_SUSPEND		0x00008000
 
 /*
  * Bits used by threaded handlers:
