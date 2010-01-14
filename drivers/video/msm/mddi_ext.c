ccb5ca31c68428136264b539ff48eb41024013a2
Daniel Walker <c_dwalke@quicinc.com>
2010-01-14 12:57:10 -0800

    Initial Contribution for msm framebuffer.
    Signed-off-by: David Brown <davidb@quicinc.com>

--- drivers/video/msm/mddi_ext.c
+++ drivers/video/msm/mddi_ext.c
@@ -31,65 +31,62 @@
 #include <linux/semaphore.h>
 #include <linux/uaccess.h>
 #include <linux/clk.h>
+#include <mach/clk.h>
 #include <linux/platform_device.h>
 
 #include "msm_fb.h"
 #include "mddihosti.h"
-#include "mddihost.h"
-#include <mach/gpio.h>
-#include <mach/clk.h>
 
-static int mddi_probe(struct platform_device *pdev);
-static int mddi_remove(struct platform_device *pdev);
+static int mddi_ext_probe(struct platform_device *pdev);
+static int mddi_ext_remove(struct platform_device *pdev);
 
-static int mddi_off(struct platform_device *pdev);
-static int mddi_on(struct platform_device *pdev);
-
-static int mddi_suspend(struct platform_device *pdev, pm_message_t state);
-static int mddi_resume(struct platform_device *pdev);
-
-#ifdef CONFIG_HAS_EARLYSUSPEND
-static void mddi_early_suspend(struct early_suspend *h);
-static void mddi_early_resume(struct early_suspend *h);
-#endif
+static int mddi_ext_off(struct platform_device *pdev);
+static int mddi_ext_on(struct platform_device *pdev);
 
 static struct platform_device *pdev_list[MSM_FB_MAX_DEV_LIST];
 static int pdev_list_cnt;
-static struct clk *mddi_clk;
-static struct mddi_platform_data *mddi_pdata;
 
-static struct platform_driver mddi_driver = {
-	.probe = mddi_probe,
-	.remove = mddi_remove,
+static int mddi_ext_suspend(struct platform_device *pdev, pm_message_t state);
+static int mddi_ext_resume(struct platform_device *pdev);
+
+#ifdef CONFIG_HAS_EARLYSUSPEND
+static void mddi_ext_early_suspend(struct early_suspend *h);
+static void mddi_ext_early_resume(struct early_suspend *h);
+#endif
+
+static struct platform_driver mddi_ext_driver = {
+	.probe = mddi_ext_probe,
+	.remove = mddi_ext_remove,
 #ifndef CONFIG_HAS_EARLYSUSPEND
 #ifdef CONFIG_PM
-	.suspend = mddi_suspend,
-	.resume = mddi_resume,
+	.suspend = mddi_ext_suspend,
+	.resume = mddi_ext_resume,
 #endif
 #endif
-	.suspend_late = NULL,
 	.resume_early = NULL,
+	.resume = NULL,
 	.shutdown = NULL,
 	.driver = {
-		   .name = "mddi",
+		   .name = "mddi_ext",
 		   },
 };
 
-extern int int_mddi_pri_flag;
+static struct clk *mddi_ext_clk;
+static struct mddi_platform_data *mddi_ext_pdata;
 
-static int mddi_off(struct platform_device *pdev)
+extern int int_mddi_ext_flag;
+
+static int mddi_ext_off(struct platform_device *pdev)
 {
 	int ret = 0;
 
 	ret = panel_next_off(pdev);
-
-	if (machine_is_msm7201a_ffa())
-		gpio_direction_output(88, 0);
+	mddi_host_stop_ext_display();
 
 	return ret;
 }
 
-static int mddi_on(struct platform_device *pdev)
+static int mddi_ext_on(struct platform_device *pdev)
 {
 	int ret = 0;
 	u32 clk_rate;
@@ -97,31 +94,29 @@ static int mddi_on(struct platform_device *pdev)
 
 	mfd = platform_get_drvdata(pdev);
 
-	if (machine_is_msm7201a_ffa())
-		gpio_direction_output(88, 1);
-
 	clk_rate = mfd->fbi->var.pixclock;
 	clk_rate = min(clk_rate, mfd->panel_info.clk_max);
 
-	if (mddi_pdata &&
-	    mddi_pdata->mddi_sel_clk &&
-	    mddi_pdata->mddi_sel_clk(&clk_rate))
-			printk(KERN_ERR
+	if (mddi_ext_pdata &&
+	    mddi_ext_pdata->mddi_sel_clk &&
+	    mddi_ext_pdata->mddi_sel_clk(&clk_rate))
+		printk(KERN_ERR
 			  "%s: can't select mddi io clk targate rate = %d\n",
 			  __func__, clk_rate);
 
-	if (clk_set_min_rate(mddi_clk, clk_rate) < 0)
+	if (clk_set_min_rate(mddi_ext_clk, clk_rate) < 0)
 		printk(KERN_ERR "%s: clk_set_min_rate failed\n",
 			__func__);
 
+	mddi_host_start_ext_display();
 	ret = panel_next_on(pdev);
 
 	return ret;
 }
 
-static int mddi_resource_initialized;
+static int mddi_ext_resource_initialized;
 
-static int mddi_probe(struct platform_device *pdev)
+static int mddi_ext_probe(struct platform_device *pdev)
 {
 	struct msm_fb_data_type *mfd;
 	struct platform_device *mdp_dev = NULL;
@@ -131,22 +126,22 @@ static int mddi_probe(struct platform_device *pdev)
 	u32 clk_rate;
 
 	if ((pdev->id == 0) && (pdev->num_resources >= 0)) {
-		mddi_pdata = pdev->dev.platform_data;
+		mddi_ext_pdata = pdev->dev.platform_data;
 
 		size =  resource_size(&pdev->resource[0]);
-		msm_pmdh_base =  ioremap(pdev->resource[0].start, size);
+		msm_emdh_base = ioremap(pdev->resource[0].start, size);
 
-		MSM_FB_INFO("primary mddi base address = 0x%x\n",
+		MSM_FB_INFO("external mddi base address = 0x%x\n",
 				pdev->resource[0].start);
 
-		if (unlikely(!msm_pmdh_base))
+		if (unlikely(!msm_emdh_base))
 			return -ENOMEM;
 
-		mddi_resource_initialized = 1;
+		mddi_ext_resource_initialized = 1;
 		return 0;
 	}
 
-	if (!mddi_resource_initialized)
+	if (!mddi_ext_resource_initialized)
 		return -EPERM;
 
 	mfd = platform_get_drvdata(pdev);
@@ -168,7 +163,7 @@ static int mddi_probe(struct platform_device *pdev)
 	// link to the latest pdev
 	/////////////////////////////////////////
 	mfd->pdev = mdp_dev;
-	mfd->dest = DISPLAY_LCD;
+	mfd->dest = DISPLAY_EXT_MDDI;
 
 	/////////////////////////////////////////
 	// alloc panel device data
@@ -176,7 +171,7 @@ static int mddi_probe(struct platform_device *pdev)
 	if (platform_device_add_data
 	    (mdp_dev, pdev->dev.platform_data,
 	     sizeof(struct msm_fb_panel_data))) {
-		printk(KERN_ERR "mddi_probe: platform_device_add_data failed!\n");
+		printk(KERN_ERR "mddi_ext_probe: platform_device_add_data failed!\n");
 		platform_device_put(mdp_dev);
 		return -ENOMEM;
 	}
@@ -184,8 +179,8 @@ static int mddi_probe(struct platform_device *pdev)
 	// data chain
 	/////////////////////////////////////////
 	pdata = mdp_dev->dev.platform_data;
-	pdata->on = mddi_on;
-	pdata->off = mddi_off;
+	pdata->on = mddi_ext_on;
+	pdata->off = mddi_ext_off;
 	pdata->next = pdev;
 
 	/////////////////////////////////////////
@@ -195,14 +190,14 @@ static int mddi_probe(struct platform_device *pdev)
 	mfd->fb_imgType = MDP_RGB_565;
 
 	clk_rate = mfd->panel_info.clk_max;
-	if (mddi_pdata &&
-	    mddi_pdata->mddi_sel_clk &&
-	    mddi_pdata->mddi_sel_clk(&clk_rate))
+	if (mddi_ext_pdata &&
+	    mddi_ext_pdata->mddi_sel_clk &&
+	    mddi_ext_pdata->mddi_sel_clk(&clk_rate))
 			printk(KERN_ERR
 			  "%s: can't select mddi io clk targate rate = %d\n",
 			  __func__, clk_rate);
 
-	if (clk_set_max_rate(mddi_clk, clk_rate) < 0)
+	if (clk_set_max_rate(mddi_ext_clk, clk_rate) < 0)
 		printk(KERN_ERR "%s: clk_set_max_rate failed\n", __func__);
 	mfd->panel_info.clk_rate = mfd->panel_info.clk_min;
 
@@ -216,135 +211,105 @@ static int mddi_probe(struct platform_device *pdev)
 	/////////////////////////////////////////
 	rc = platform_device_add(mdp_dev);
 	if (rc)
-		goto mddi_probe_err;
+		goto mddi_ext_probe_err;
 
 	pdev_list[pdev_list_cnt++] = pdev;
 
 #ifdef CONFIG_HAS_EARLYSUSPEND
-	mfd->mddi_early_suspend.level = EARLY_SUSPEND_LEVEL_DISABLE_FB;
-	mfd->mddi_early_suspend.suspend = mddi_early_suspend;
-	mfd->mddi_early_suspend.resume = mddi_early_resume;
-	register_early_suspend(&mfd->mddi_early_suspend);
+	mfd->mddi_ext_early_suspend.level = EARLY_SUSPEND_LEVEL_DISABLE_FB;
+	mfd->mddi_ext_early_suspend.suspend = mddi_ext_early_suspend;
+	mfd->mddi_ext_early_suspend.resume = mddi_ext_early_resume;
+	register_early_suspend(&mfd->mddi_ext_early_suspend);
 #endif
 
 	return 0;
 
-mddi_probe_err:
+mddi_ext_probe_err:
 	platform_device_put(mdp_dev);
 	return rc;
 }
 
-static int mddi_pad_ctrl;
-static int mddi_power_locked;
-static int mddi_is_in_suspend;
+static int mddi_ext_is_in_suspend;
 
-void mddi_disable(int lock)
+static int mddi_ext_suspend(struct platform_device *pdev, pm_message_t state)
 {
-	mddi_host_type host_idx = MDDI_HOST_PRIM;
-
-	if (mddi_power_locked)
-		return;
-
-	if (lock)
-		mddi_power_locked = 1;
-
-	mddi_pad_ctrl = mddi_host_reg_in(PAD_CTL);
-	mddi_host_reg_out(PAD_CTL, 0x0);
-
-	if (clk_set_min_rate(mddi_clk, 0) < 0)
-		printk(KERN_ERR "%s: clk_set_min_rate failed\n", __func__);
-
-	clk_disable(mddi_clk);
-	disable_irq(INT_MDDI_PRI);
-
-	if (mddi_pdata && mddi_pdata->mddi_power_save)
-		mddi_pdata->mddi_power_save(0);
-}
-
-static int mddi_suspend(struct platform_device *pdev, pm_message_t state)
-{
-	if (mddi_is_in_suspend)
+	if (mddi_ext_is_in_suspend)
 		return 0;
 
-	mddi_is_in_suspend = 1;
-	mddi_disable(0);
+	mddi_ext_is_in_suspend = 1;
+
+	if (clk_set_min_rate(mddi_ext_clk, 0) < 0)
+		printk(KERN_ERR "%s: clk_set_min_rate failed\n", __func__);
+
+	clk_disable(mddi_ext_clk);
+	disable_irq(INT_MDDI_EXT);
+
 	return 0;
 }
 
-static int mddi_resume(struct platform_device *pdev)
+static int mddi_ext_resume(struct platform_device *pdev)
 {
-	mddi_host_type host_idx = MDDI_HOST_PRIM;
+	struct msm_fb_data_type *mfd;
 
-	if (!mddi_is_in_suspend)
+	mfd = platform_get_drvdata(pdev);
+
+	if (!mddi_ext_is_in_suspend)
 		return 0;
 
-	mddi_is_in_suspend = 0;
+	mddi_ext_is_in_suspend = 0;
+	enable_irq(INT_MDDI_EXT);
 
-	if (mddi_power_locked)
-		return 0;
-
-	if (mddi_pdata && mddi_pdata->mddi_power_save)
-		mddi_pdata->mddi_power_save(1);
-
-	mddi_host_reg_out(PAD_CTL, mddi_pad_ctrl);
-	enable_irq(INT_MDDI_PRI);
-	clk_enable(mddi_clk);
+	clk_enable(mddi_ext_clk);
 
 	return 0;
 }
 
 #ifdef CONFIG_HAS_EARLYSUSPEND
-static void mddi_early_suspend(struct early_suspend *h)
+static void mddi_ext_early_suspend(struct early_suspend *h)
 {
 	pm_message_t state;
 	struct msm_fb_data_type *mfd = container_of(h, struct msm_fb_data_type,
-							mddi_early_suspend);
+							mddi_ext_early_suspend);
 
 	state.event = PM_EVENT_SUSPEND;
-	mddi_suspend(mfd->pdev, state);
+	mddi_ext_suspend(mfd->pdev, state);
 }
 
-static void mddi_early_resume(struct early_suspend *h)
+static void mddi_ext_early_resume(struct early_suspend *h)
 {
 	struct msm_fb_data_type *mfd = container_of(h, struct msm_fb_data_type,
-							mddi_early_suspend);
-	mddi_resume(mfd->pdev);
+							mddi_ext_early_suspend);
+	mddi_ext_resume(mfd->pdev);
 }
 #endif
 
-extern struct timer_list mddi_host_timer;
-
-static int mddi_remove(struct platform_device *pdev)
+static int mddi_ext_remove(struct platform_device *pdev)
 {
-	if (mddi_host_timer.function)
-		del_timer_sync(&mddi_host_timer);
-
-	iounmap(msm_pmdh_base);
-
+	iounmap(msm_emdh_base);
 	return 0;
 }
 
-static int mddi_register_driver(void)
+static int mddi_ext_register_driver(void)
 {
-	return platform_driver_register(&mddi_driver);
+	return platform_driver_register(&mddi_ext_driver);
 }
 
-static int __init mddi_driver_init(void)
+static int __init mddi_ext_driver_init(void)
 {
 	int ret;
 
-	mddi_clk = clk_get(NULL, "mddi_clk");
-	if (IS_ERR(mddi_clk)) {
-		printk(KERN_ERR "can't find mddi_clk \n");
-		return PTR_ERR(mddi_clk);
+	mddi_ext_clk = clk_get(NULL, "emdh_clk");
+	if (IS_ERR(mddi_ext_clk)) {
+		printk(KERN_ERR "can't find emdh_clk\n");
+		return PTR_ERR(mddi_ext_clk);
 	}
-	clk_enable(mddi_clk);
+	clk_enable(mddi_ext_clk);
 
-	ret = mddi_register_driver();
+	ret = mddi_ext_register_driver();
 	if (ret) {
-		clk_disable(mddi_clk);
-		clk_put(mddi_clk);
-		printk(KERN_ERR "mddi_register_driver() failed!\n");
+		clk_disable(mddi_ext_clk);
+		clk_put(mddi_ext_clk);
+		printk(KERN_ERR "mddi_ext_register_driver() failed!\n");
 		return ret;
 	}
 	mddi_init();
@@ -352,4 +317,4 @@ static int __init mddi_driver_init(void)
 	return ret;
 }
 
-module_init(mddi_driver_init);
+module_init(mddi_ext_driver_init);
