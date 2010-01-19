199c423e532bfdaa30601782618b88a7185812b7
David Brown <davidb@quicinc.com>
2010-01-19 09:03:39 -0800

    Device data for MSM boardfile support.
    Needed to break cyclic dependencies with the platform board
    files.
    
    Signed-off-by: David Brown <davidb@quicinc.com>
    Signed-off-by: David Brown <davidb@quicinc.com>

--- arch/arm/mach-msm/include/mach/board.h
+++ arch/arm/mach-msm/include/mach/board.h
@@ -20,12 +20,10 @@
 
 #include <linux/types.h>
 #include <linux/input.h>
-#include <linux/clk.h>
+#include <linux/usb.h>
 
 /* platform device data structures */
-
-struct msm_acpu_clock_platform_data
-{
+struct msm_acpu_clock_platform_data {
 	uint32_t acpu_switch_time_us;
 	uint32_t max_speed_delta_khz;
 	uint32_t vdd_switch_time_us;
@@ -34,6 +32,98 @@ struct msm_acpu_clock_platform_data
 	unsigned int max_axi_khz;
 	unsigned int max_vdd;
 	int (*acpu_set_vdd) (int mvolts);
+};
+
+struct msm_camera_io_ext {
+	uint32_t mdcphy;
+	uint32_t mdcsz;
+	uint32_t appphy;
+	uint32_t appsz;
+	uint32_t camifpadphy;
+	uint32_t camifpadsz;
+};
+
+struct msm_camera_device_platform_data {
+	void (*camera_gpio_on) (void);
+	void (*camera_gpio_off)(void);
+	struct msm_camera_io_ext ioext;
+};
+
+#ifdef CONFIG_SENSORS_MT9T013
+struct msm_camera_legacy_device_platform_data {
+	int sensor_reset;
+	int sensor_pwd;
+	int vcm_pwd;
+	void (*config_gpio_on) (void);
+	void (*config_gpio_off)(void);
+};
+#endif
+
+#define MSM_CAMERA_FLASH_NONE 0
+#define MSM_CAMERA_FLASH_LED  1
+
+struct msm_camera_sensor_info {
+	const char *sensor_name;
+	int sensor_reset;
+	int sensor_pwd;
+	int vcm_pwd;
+	int mclk;
+	int flash_type;
+	struct msm_camera_device_platform_data *pdata;
+	struct resource *resource;
+	uint8_t num_resources;
+};
+
+struct clk;
+
+struct snd_endpoint {
+	int id;
+	const char *name;
+};
+
+struct msm_snd_endpoints {
+	struct snd_endpoint *endpoints;
+	unsigned num;
+};
+
+/* 7k target ADSP information */
+/* Bit 23:0, for codec identification like mp3, wav etc *
+ * Bit 27:24, for mode identification like tunnel, non tunnel*
+ * bit 31:28, for operation support like DM, DMA */
+enum msm_adspdec_concurrency {
+	MSM_ADSP_CODEC_WAV = 0,
+	MSM_ADSP_CODEC_ADPCM = 1,
+	MSM_ADSP_CODEC_MP3 = 2,
+	MSM_ADSP_CODEC_REALAUDIO = 3,
+	MSM_ADSP_CODEC_WMA = 4,
+	MSM_ADSP_CODEC_AAC = 5,
+	MSM_ADSP_CODEC_RESERVED = 6,
+	MSM_ADSP_CODEC_MIDI = 7,
+	MSM_ADSP_CODEC_YADPCM = 8,
+	MSM_ADSP_CODEC_QCELP = 9,
+	MSM_ADSP_CODEC_AMRNB = 10,
+	MSM_ADSP_CODEC_AMRWB = 11,
+	MSM_ADSP_CODEC_EVRC = 12,
+	MSM_ADSP_CODEC_WMAPRO = 13,
+	MSM_ADSP_MODE_TUNNEL = 24,
+	MSM_ADSP_MODE_NONTUNNEL = 25,
+	MSM_ADSP_OP_DMA = 28,
+	MSM_ADSP_OP_DM = 29,
+};
+
+struct msm_adspdec_info {
+	const char *module_name;
+	unsigned module_queueid;
+	int module_decid; /* objid */
+	unsigned nr_codec_support;
+};
+
+struct msm_adspdec_database {
+	unsigned num_dec;
+	unsigned num_concurrency_support;
+	unsigned int *dec_concurrency_table; /* Bit masked entry to *
+					      *	represents codec, mode etc */
+	struct msm_adspdec_info  *dec_info_list;
 };
 
 struct msm_panel_common_pdata {
@@ -57,7 +147,6 @@ struct tvenc_platform_data {
 struct mddi_platform_data {
 	void (*mddi_power_save)(int on);
 	int (*mddi_sel_clk)(u32 *clk_rate);
-	int (*mddi_power_on)(int);
 };
 
 struct msm_fb_platform_data {
@@ -67,6 +156,14 @@ struct msm_fb_platform_data {
 
 struct msm_i2c_platform_data {
 	int clk_freq;
+	uint32_t rmutex;
+	const char *rsl_id;
+	uint32_t pm_lat;
+	int pri_clk;
+	int pri_dat;
+	int aux_clk;
+	int aux_dat;
+	void (*msm_i2c_config_gpio)(int iface, int config_type);
 };
 
 /* common init routines for use by arch/arm/mach-msm/board-*.c */
@@ -80,11 +177,25 @@ void __init msm_init_irq(void);
 void __init msm_clock_init(struct clk *clock_tbl, unsigned num_clocks);
 void __init msm_acpu_clock_init(struct msm_acpu_clock_platform_data *);
 
-#if defined(CONFIG_USB_FUNCTION_MSM_HSUSB)
+struct mmc_platform_data;
+int __init msm_add_sdcc(unsigned int controller,
+		struct mmc_platform_data *plat);
+int __init rmt_storage_add_ramfs(void);
+
+struct msm_usb_host_platform_data;
+int __init msm_add_host(unsigned int host,
+		struct msm_usb_host_platform_data *plat);
+#if defined(CONFIG_USB_FUNCTION_MSM_HSUSB) || defined(CONFIG_USB_MSM_72K)
 void msm_hsusb_set_vbus_state(int online);
 #else
 static inline void msm_hsusb_set_vbus_state(int online) {}
 #endif
+
+void __init msm_snddev_init(void);
+void msm_snddev_poweramp_on(void);
+void msm_snddev_poweramp_off(void);
+void msm_snddev_hsed_pamp_on(void);
+void msm_snddev_hsed_pamp_off(void);
 
 extern int msm_shared_ram_phys; /* defined in arch/arm/mach-msm/io.c */
 
