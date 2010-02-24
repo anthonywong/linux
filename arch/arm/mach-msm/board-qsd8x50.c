b18100accabb8e3b3579c717cabdf78993aee8b6
Premal Shah <pshah@quicinc.com>
2010-02-24 15:18:13 -0800

    pmem: mark pmem_adsp region as cached
    Flash10 player on ChromeOS does color conversion in the cpu and hence
    for performance reasons we want to make the decoder buffers as cached.
    Subsystems that use pmem_adsp region (vdec/camera) can still use
    this area by using O_SYNC flag which will give the original
    uncached behavior.
    
    Change-Id: Ibfe098ff047d945e80a754b3fbf78d313fe81788
    Signed-off-by: Premal Shah <pshah@quicinc.com>
    (cherry picked from commit 5a32ba2881fd764a8467368208b9537a7d39b0b6)

--- arch/arm/mach-msm/board-qsd8x50.c
+++ arch/arm/mach-msm/board-qsd8x50.c
@@ -735,7 +735,7 @@ static struct android_pmem_platform_data android_pmem_pdata = {
 static struct android_pmem_platform_data android_pmem_adsp_pdata = {
 	.name = "pmem_adsp",
 	.allocator_type = PMEM_ALLOCATORTYPE_BITMAP,
-	.cached = 0,
+	.cached = 1,
 };
 
 #ifdef CONFIG_PMEM_GPU0
