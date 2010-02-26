be6c2cd3bb04b39e2bb8ad5c0a833d64e7f54a3e
Matt Wagantall <mattw@quicinc.com>
2010-02-26 12:33:46 -0800

    Revert "msm: clock: Remove unsupported 'usb_phy_clk' from 7x27 and 8x50."
    Adding back the 'usb_phy_clk' for use only with clock reset API.
    
    This reverts commit 0b9767da5993ab7e50b9cdb30a05e0db84f452b4.
    
    Conflicts:
    	arch/arm/mach-msm/devices.c
    
    Signed-off-by: Matt Wagantall <mattw@quicinc.com>
    (cherry picked from commit 90194afbcc54cdef3a861543d08fdc6af9e9f1f8)

--- arch/arm/mach-msm/devices.c
+++ arch/arm/mach-msm/devices.c
@@ -1222,6 +1222,7 @@ struct clk msm_clocks_7x27[] = {
 	CLK_PCOM("usb_hs_clk",	USB_HS_CLK,	NULL, OFF),
 	CLK_PCOM("usb_hs_pclk",	USB_HS_PCLK,	NULL, OFF),
 	CLK_PCOM("usb_otg_clk",	USB_OTG_CLK,	NULL, 0),
+	CLK_PCOM("usb_phy_clk",	USB_PHY_CLK,	NULL, 0),
 	CLK_PCOM("vdc_clk",	VDC_CLK,	NULL, OFF | CLK_MIN),
 	CLK_PCOM("vfe_clk",	VFE_CLK,	NULL, OFF),
 	CLK_PCOM("vfe_mdc_clk",	VFE_MDC_CLK,	NULL, OFF),
@@ -1356,6 +1357,7 @@ struct clk msm_clocks_8x50[] = {
 	CLK_PCOM("usb_hs2_pclk",	USB_HS2_PCLK,	NULL, OFF),
 	CLK_PCOM("usb_hs3_clk",	USB_HS3_CLK,	NULL, OFF),
 	CLK_PCOM("usb_hs3_pclk",	USB_HS3_PCLK,	NULL, OFF),
+	CLK_PCOM("usb_phy_clk",	USB_PHY_CLK,	NULL, 0),
 
 #ifdef CONFIG_MSM_SOC_REV_A
 	CLK_PCOM("grp_2d_clk",	GRP_2D_CLK,	NULL, 0),
