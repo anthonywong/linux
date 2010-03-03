f26dba18217e43576b86d5d0b503b261acaa0bd0
Peter Lohmann <plohmann@quicinc.com>
2010-03-02 18:20:21 -0800

    qdsp6: add audiov2 driver
    This driver will support:
    * PCM playback and capture
    * device switching
    * volume control, mute/un-mute
    * voice call
    
    Signed-off-by: Peter Lohmann <plohmann@quicinc.com>
    (cherry picked from commit 501e72fbafb7c0fa3d7eeda4ee01d430dfc11fc3)

--- arch/arm/mach-msm/include/mach/debug_adsp_mm.h
+++ arch/arm/mach-msm/include/mach/debug_adsp_mm.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2009, Code Aurora Forum. All rights reserved.
+/* Copyright (c) 2009-2010, Code Aurora Forum. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -25,60 +25,21 @@
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  */
-
-#ifndef __MSM_AUDIO_QCP_H
-#define __MSM_AUDIO_QCP_H
-
-#include <linux/msm_audio.h>
-
-#define AUDIO_SET_QCELP_ENC_CONFIG  _IOW(AUDIO_IOCTL_MAGIC, \
-	0, struct msm_audio_qcelp_enc_config)
-
-#define AUDIO_GET_QCELP_ENC_CONFIG  _IOR(AUDIO_IOCTL_MAGIC, \
-	1, struct msm_audio_qcelp_enc_config)
-
-#define AUDIO_SET_EVRC_ENC_CONFIG  _IOW(AUDIO_IOCTL_MAGIC, \
-	2, struct msm_audio_evrc_enc_config)
-
-#define AUDIO_GET_EVRC_ENC_CONFIG  _IOR(AUDIO_IOCTL_MAGIC, \
-	3, struct msm_audio_evrc_enc_config)
-
-#define CDMA_RATE_BLANK		0x00
-#define CDMA_RATE_EIGHTH	0x01
-#define CDMA_RATE_QUARTER	0x02
-#define CDMA_RATE_HALF		0x03
-#define CDMA_RATE_FULL		0x04
-#define CDMA_RATE_ERASURE	0x05
-
-struct msm_audio_qcelp_enc_config {
-	uint32_t cdma_rate;
-	uint32_t min_bit_rate;
-	uint32_t max_bit_rate;
-};
-
-struct msm_audio_evrc_enc_config {
-	uint32_t cdma_rate;
-	uint32_t min_bit_rate;
-	uint32_t max_bit_rate;
-};
-
-/* legacy structs */
-struct msm_audio_qcelp_config {
-	uint32_t channels;
-	uint32_t cdma_rate;
-	uint32_t min_bit_rate;
-	uint32_t max_bit_rate;
-};
-
-struct msm_audio_evrc_config {
-	uint32_t channels;
-	uint32_t cdma_rate;
-	uint32_t min_bit_rate;
-	uint32_t max_bit_rate;
-	uint8_t bit_rate_reduction;
-	uint8_t hi_pass_filter;
-	uint8_t noise_suppressor;
-	uint8_t post_filter;
-};
-
-#endif /* __MSM_AUDIO_QCP_H */
+#ifndef __ARCH_ARM_MACH_MSM_DEBUG_ADSP_MM_H_
+#define __ARCH_ARM_MACH_MSM_DEBUG_ADSP_MM_H_
+/* ************************DEBUG LEVELS*************************
+ * 0 - Disables all the messages including error messages
+ * 1 - Prints error messages only
+ * 2 - Prints both error and info messages (Default)
+ * 3 - Prints all the messages including debug messages
+ * ************************************************************/
+/* Change the below defined debug level according to the levels
+ * defined above.
+ *
+ * The debug level defined below will effect all the files across
+ * this module i.e. adsp module.
+ */
+#define MSM_MM_DEBUG_LEVEL 2
+#define MSM_MM_MODULE "adsp" /* module name used in log messages */
+#include <mach/debug_mm.h>
+#endif /* __ARCH_ARM_MACH_MSM_DEBUG_ADSP_MM_H_ */
