nxp-imx/linux-imx

ENGR00116779 OneWire: Clear the information when removing the module
1.When inserting OneWire module at the second time, the error is reported.
This is because irq information isnot cleaned when removing the module.
2. Correct error handling in probe function.

Signed-off-by: Lily Zhang <r58066@freescale.com>

drivers/w1/masters/mxc_w1.c
@@ -332,7 +332,7 @@ static int __devinit mxc_w1_probe(struct platform_device *pdev)
 	struct mxc_w1_device *dev;
 	struct mxc_w1_config *data =
 	    (struct mxc_w1_config *)pdev->dev.platform_data;
-	int flag, ret_val, irq;
+	int flag, ret_val, irq = -ENXIO;
 	int err = 0;
 	ret_val = 0;
 	flag = data->search_rom_accelerator;
@@ -356,23 +356,30 @@ static int __devinit mxc_w1_probe(struct platform_device *pdev)
 		dev->bus_master->read_byte = &mxc_w1_ds2_read_byte;
 		dev->bus_master->search = &search_ROM_accelerator;
 		irq = platform_get_irq(pdev, 0);
+		if (irq < 0) {
+			err = -ENOENT;
+			goto err_out_free_device;
+		}
 		ret_val =
 		    request_irq(irq, w1_interrupt_handler, 0, "mxc_w1", dev);
 		if (ret_val) {
 			pr_debug("OWire:request_irq(%d) returned error %d\n",
 				 irq, ret_val);
-			return -1;
+			err = -1;
+			goto err_out_free_device;
 		}
 	}
 	err = w1_add_master_device(dev->bus_master);
 	if (err)
-		goto err_out_free_device;
+		goto err_irq;
 
 	platform_set_drvdata(pdev, dev);
 	return 0;
 
-      err_out_free_device:
-
+err_irq:
+	if (irq >= 0)
+		free_irq(irq, dev);
+err_out_free_device:
 	kfree(dev);
 	return err;
 }
@@ -385,12 +392,22 @@ static int __devinit mxc_w1_probe(struct platform_device *pdev)
 static int mxc_w1_remove(struct platform_device *pdev)
 {
 	struct mxc_w1_device *dev = platform_get_drvdata(pdev);
+	struct mxc_w1_config *data =
+	    (struct mxc_w1_config *)pdev->dev.platform_data;
+	int irq;
 
-	clk_disable(dev->clk);
-	clk_put(dev->clk);
 	if (dev->found) {
 		w1_remove_master_device(dev->bus_master);
 	}
+
+	irq = platform_get_irq(pdev, 0);
+	if ((irq >= 0) && (data->search_rom_accelerator))
+		free_irq(irq, dev);
+
+	clk_disable(dev->clk);
+	clk_put(dev->clk);
+
+	kfree(dev);
 	platform_set_drvdata(pdev, NULL);
 
 	return 0;

anthonywong/linux

Signed-off-by: Bryan Wu <bryan.wu@canonical.com>

drivers/w1/masters/mxc_w1.c
@@ -296,6 +296,10 @@ static int __init mxc_w1_probe(struct platform_device *pdev)
 		mdev->bus_master.read_byte = &mxc_w1_ds2_read_byte;
 		mdev->bus_master.search = &search_ROM_accelerator;
 		irq = platform_get_irq(pdev, 0);
+		if (irq < 0) {
+			err = -ENOENT;
+			goto failed_irq; 
+		}
 		err = request_irq(irq, w1_interrupt_handler, 0, "mxc_w1", mdev);
 		if (err) {
 			pr_debug("OWire:request_irq(%d) returned error %d\n",
@@ -333,6 +337,9 @@ static int mxc_w1_remove(struct platform_device *pdev)
 {
 	struct mxc_w1_device *mdev = platform_get_drvdata(pdev);
 	struct resource *res;
+	struct mxc_w1_config *data =
+	    (struct mxc_w1_config *)pdev->dev.platform_data;
+	int irq;
 
 	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
 
@@ -340,6 +347,11 @@ static int mxc_w1_remove(struct platform_device *pdev)
 
 	iounmap(mdev->regs);
 	release_mem_region(res->start, resource_size(res));
+
+	irq = platform_get_irq(pdev, 0);
+	if ((irq >= 0) && (data->search_rom_accelerator))
+		free_irq(irq, mdev);
+
 	clk_disable(mdev->clk);
 	clk_put(mdev->clk);
 