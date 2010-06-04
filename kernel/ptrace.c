75030b6b80d5afba26f5146df5e74832ebb3e333
Kees Cook <kees.cook@canonical.com>
2010-06-04 11:14:27 -0700

    ptrace: limit scope to attach only (allow read)
    The modification to may_ptrace_access is to broad, and would block
    access to things in /proc/$pid/ that many applications expect to be
    able to see.  Since the goal is to stop arbitrary code injection,
    allow PTRACE_MODE_READ but not PTRACE_MODE_ATTACH for non-children.
    
    BugLink: https://launchpad.net/bugs/589656
    
    Signed-off-by: Kees Cook <kees.cook@canonical.com>
    Signed-off-by: Leann Ogasawara <leann.ogasawara@canonical.com>

--- kernel/ptrace.c
+++ kernel/ptrace.c
@@ -156,8 +156,9 @@ int __ptrace_may_access(struct task_struct *task, unsigned int mode)
 		dumpable = get_dumpable(task->mm);
 	if (!dumpable && !capable(CAP_SYS_PTRACE))
 		return -EPERM;
-	if (ptrace_scope && !capable(CAP_SYS_PTRACE)) {
-		/* require ptrace target be a child of ptracer */
+	if (mode == PTRACE_MODE_ATTACH && ptrace_scope &&
+            !capable(CAP_SYS_PTRACE)) {
+		/* require ptrace target be a child of ptracer on attach */
 		struct task_struct *tmp = task;
 		struct task_struct *curtemp = current;
 		int rc = 0;
