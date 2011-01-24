9503cf9bead965498e8118f0a3ca443b6c2671a6
Andy Whitcroft <apw@canonical.com>
2011-01-24 11:54:28 +0000

    Revert "Input: sysrq -- ensure sysrq_enabled and __sysrq_enabled are consistent"
    This reverts commit 524dfc5ccc83c1de4331955b1fa054041f3a8834.
    
    Signed-off-by: Andy Whitcroft <apw@canonical.com>

--- drivers/tty/sysrq.c
+++ drivers/tty/sysrq.c
@@ -46,7 +46,7 @@
 #include <asm/irq_regs.h>
 
 /* Whether we react on sysrq keys or just ignore them */
-static int __read_mostly sysrq_enabled = CONFIG_MAGIC_SYSRQ_DEFAULT;
+static int __read_mostly sysrq_enabled = 1;
 static bool __read_mostly sysrq_always_enabled;
 
 static bool sysrq_on(void)
